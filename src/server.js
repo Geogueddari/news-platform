531285a9798b0fc613f42633bdd79fa1845dd197
Geogueddari <abdeljabbarelgaddari24@gmail.com>
2025-01-05 00:36:32 +0100

    ameliorate general errors handling

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
 const newsRoutes = require('./routes/newsRoutes');
 const authRoutes = require('./routes/authRoutes');
 const usersRoutes = require('./routes/usersRoutes');
@@ -16,19 +17,15 @@ app.use(express.static('public'));
 
 app.use('/api/authentification',authRoutes);
 app.use('/api/users' , usersRoutes);
-// Routes
 app.use('/api/news', newsRoutes);
 
-// TODO: Question 3 - Ajouter un middleware pour gérer les erreurs
-app.use((err, req, res) => {
-  if(err.message){
-    console.error(err.stack);
-    const message = 'Internal Server Error';
-    res.status(err.status || 500).json({message});
-  }
+app.use((req, res) => {
+  res.status(404).json({ message: "Route non trouvée" });
 });
-
-
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Erreur interne du serveur" });
+});
 
 
 app.listen(PORT, () => {
